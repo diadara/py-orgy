ca4c11819db0853c86ffa3a9efd3fa268f563b01
Nithin Saji <nithin@vaio>
2012-07-17 13:04:32 +0530

    started work on the python scrit

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+"""This is a simple script to organize files within your download folder
+Author:Nithin Saji
+Name:Py-Orgy
+License:CC
+"""
+import os
+print "time to clean.....";
+
